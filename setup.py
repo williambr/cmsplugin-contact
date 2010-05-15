a7e7821af632b2b62966606ed9029f524a2b813a
Sergey Generalov <sergey@genbit.ru>
2010-05-15 02:51:01 -0700

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin-contact',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
