942018ad68a6fb6c1777c21ec9f55bb0decaa496
maccesch <maccesch@web.de>
2011-01-14 07:53:17 +0100

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmsplugin-contact',
-    version='0.9.2',
+    version='0.9.3',
     description='Contact form plugin for Django CMS with spam protection and i18n',
     author='Maccesch',
     author_email='maccesch@gmail.com',
