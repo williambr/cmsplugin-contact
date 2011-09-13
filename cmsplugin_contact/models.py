19dca8cdaaff3791a1a5ffd4ee4f050673ac93ca
Tomas Neme <lacrymology@gmail.com>
2011-09-13 18:33:19 -0300

    style fix

--- cmsplugin_contact/models.py
+++ cmsplugin_contact/models.py
@@ -19,19 +19,32 @@ class BaseContact(CMSPlugin):
     )
     
     site_email = models.EmailField(_('Email recipient'))
-    email_label = models.CharField(_('Email sender label'), default=_('Your email address'), max_length=100)
-    subject_label = models.CharField(_('Subject label'), default=_('Subject'), max_length=200)
-    content_label = models.CharField(_('Message content label'), default=_('Message'), max_length=100)
-    thanks = models.TextField(verbose_name=_("Thanks message"), help_text=_('Message displayed on successful submit'), default=_('Thank you for your message.'), max_length=200)
-    submit = models.CharField(_('Submit button value'), default=_('Submit'), max_length=30)
+    email_label = models.CharField(_('Email sender label'),
+                                   default=_('Your email address'),
+                                   max_length=100)
+    subject_label = models.CharField(_('Subject label'),
+                                     default=_('Subject'), max_length=200)
+    content_label = models.CharField(_('Message content label'),
+                                     default=_('Message'), max_length=100)
+    thanks = models.TextField(
+        verbose_name=_("Thanks message"),
+        help_text=_('Message displayed on successful submit'),
+        default=_('Thank you for your message.'), max_length=200)
+    submit = models.CharField(_('Submit button value'),
+                              default=_('Submit'), max_length=30)
     
-    spam_protection_method = models.SmallIntegerField(verbose_name=_('Spam protection method'), choices=SPAM_PROTECTION_CHOICES, default=0)
+    spam_protection_method = models.SmallIntegerField(
+        verbose_name=_('Spam protection method'),
+        choices=SPAM_PROTECTION_CHOICES, default=0)
     
     akismet_api_key = models.CharField(max_length=255, blank=True)
     
     recaptcha_public_key = models.CharField(max_length=255, blank=True)
     recaptcha_private_key = models.CharField(max_length=255, blank=True)
-    recaptcha_theme = models.CharField(max_length=20, choices=THEME_CHOICES, default='clean', verbose_name=_('ReCAPTCHA theme'))
+    recaptcha_theme = models.CharField(max_length=20,
+                                       choices=THEME_CHOICES,
+                                       default='clean',
+                                       verbose_name=_('ReCAPTCHA theme'))
 
     class Meta:
         abstract = True
